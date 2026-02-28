10690a4f7714ccf96e4a1091478d7a300ea744bb
Jan's CachyOS <honza.hruza.d5@gmail.com>
2026-02-28 22:34:37 +0100

    Added a simple ANSI definitions file.

---
+++ RallyDakar/inc/Ansi.h
@@ -0,0 +1,15 @@
+//
+// Created by jan on 28/02/2026.
+//
+
+#ifndef RALLYDAKAR_ANSI_H
+#define RALLYDAKAR_ANSI_H
+
+#define RESET          "\033[0m"
+#define ACCENT_TEXT     "\033[38;5;11m"
+#define ACCENT_BACK     "\033[48;5;11m\033[38;5;0m"
+#define INVERT_VIDEO    "\033[7m"
+#define BLINK_SLOW      "\033[5m"
+#define BOLD            "\033[1m"
+
+#endif //RALLYDAKAR_ANSI_H
