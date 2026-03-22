0a4988c5294cd5b96475603150a31646ff3905de
Jan's CachyOS <honza.hruza.d5@gmail.com>
2026-03-22 16:25:57 +0100

    Added basic help option.

--- MotoGP/main.c
+++ MotoGP/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "inc/Common.h"
 #include "inc/GameSession.h"
@@ -7,7 +8,25 @@
 #include "inc/Debug.h"
 #include "inc/UI/AppMenus.h"
 
+#define CMD_HELP        "--help"
+
+/**
+ * @brief Representing the global active game session pointer.
+ */
 GameSession *pSession = NULL;
+
+static void ShowHelp(void) {
+    // prints the help/about
+    CoDrawBanner();
+    printf("\n");
+
+    printf("%sABOUT %s%s\n", BOLD, PRODUCT_NAME, RESET);
+    puts("This app is a simple random MotoGP manager. It was made solely for learning purposes.\n");
+
+    printf("%sCREDITS%s\n", BOLD, RESET);
+    printf("Made with %s\u2764%s by %s!\n", ACCENT_TEXT, RESET, AUTHOR);
+    return;
+}
 
 static void AppInit(void) {
     debug_open();
@@ -40,6 +59,14 @@ static void Test(void) {
 
 int main(int argc, const char **argv) {
     if (argc > 1) {
+
+        // help is allowed
+        if (strcmp(argv[1], CMD_HELP) == 0) {
+            ShowHelp();
+            return EXIT_SUCCESS;
+        }
+
+        // arguments are disabled
         fprintf(stderr, "%s: Arguments are disabled.\n", argv[0]);
         return EXIT_FAILURE;
     }
