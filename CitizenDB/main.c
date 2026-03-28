60581b6b8ba1293965855b9c49f2ca3f6c9b623f
Jan's CachyOS <honza.hruza.d5@gmail.com>
2026-03-28 23:38:29 +0100

    File cleanups.

--- CitizenDB/main.c
+++ CitizenDB/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 #include "inc/UI/Ansi.h"
 #include "inc/Common.h"
-#include "inc/Person.h"
 
 STATUS AppInit(void) {
     srand(time(NULL));
@@ -24,30 +22,7 @@ int main(void) {
         fprintf(stderr, "Initialization failed.\n");
     }
 
-    Person p;
-    memset(&p, 0, sizeof(Person));
-
-    // firstname
-    printf("Enter your first name: ");
-    scanf("%s", &p.Firstname);
-
-    // lastname
-    printf("Enter your last name: ");
-    scanf("%s", &p.Lastname);
-
-    // auto-generated fields
-    p.Id = time(NULL);
-    p.Age = rand() % 99 + 1;
-    snprintf(p.CountryCode, 4, "%s", gCountries[rand() % COUNTRIES_COUNT]);
-    printf("\n");
-
-    // print user
-    printf("First name:: %s\n", p.Firstname);
-    printf("Last name:   %s\n", p.Lastname);
-    printf("Age:         %d\n", p.Age);
-    printf("Country:     %s\n", p.CountryCode);
-
-    getchar();
+    // TODO main logic goes here
 
     if (AppCleanup() != STATUS_OK) {
         fprintf(stderr, "Cleanup failed.\n");
